bcaefdd34dfd4bf7e1cf8a107584f435cdb22ed3
MananTheCoderKid <78912063+MananTheCoderKid@users.noreply.github.com>
2021-07-02 17:15:06 +0530

    Add files via upload

---
+++ BPotato.js
@@ -0,0 +1,79 @@
+function hide() {
+    document.getElementById("ingredients_div").style.display = "none";
+}
+
+function show() {
+    document.getElementById("ingredients_div").style.display = "inline-block"
+}
+
+function hide1() {
+    document.getElementById("step1_div").style.display = "none";
+}
+
+function show1() {
+    document.getElementById("step1_div").style.display = "inline-block"
+}
+
+function hide2() {
+    document.getElementById("step2_div").style.display = "none";
+}
+
+function show2() {
+    document.getElementById("step2_div").style.display = "inline-block"
+}
+
+function hide3() {
+    document.getElementById("step3_div").style.display = "none";
+}
+
+function show3() {
+    document.getElementById("step3_div").style.display = "inline-block"
+}
+
+function hide4() {
+    document.getElementById("step4_div").style.display = "none";
+}
+
+function show4() {
+    document.getElementById("step4_div").style.display = "inline-block"
+}
+
+function hide5() {
+    document.getElementById("step5_div").style.display = "none";
+}
+
+function show5() {
+    document.getElementById("step5_div").style.display = "inline-block"
+}
+
+function hide6() {
+    document.getElementById("step6_div").style.display = "none";
+}
+
+function show6() {
+    document.getElementById("step6_div").style.display = "inline-block"
+}
+
+function hide7() {
+    document.getElementById("step7_div").style.display = "none";
+}
+
+function show7() {
+    document.getElementById("step7_div").style.display = "inline-block"
+}
+
+function hide8() {
+    document.getElementById("step8_div").style.display = "none";
+}
+
+function show8() {
+    document.getElementById("step8_div").style.display = "inline-block"
+}
+
+function hide9() {
+    document.getElementById("step9_div").style.display = "none";
+}
+
+function show9() {
+    document.getElementById("step9_div").style.display = "inline-block"
+}
---
+++ CAsia.js
@@ -0,0 +1,73 @@
+//ingredients
+function hide() {
+    document.getElementById("ingredients_div").style.display = "none";
+    console.log("Ingredients Are Hidden - Double click to show");
+}
+
+function show() {
+    document.getElementById("ingredients_div").style.display = "inline-block";
+    console.log("Ingredients Are Shown - Single click to hide");
+}
+//step1
+function hide1() {
+    document.getElementById("step1_div").style.display = "none";
+    console.log("Step 1 is Hidden - Double click to show");
+}
+
+function show1() {
+    document.getElementById("step1_div").style.display = "inline-block";
+    console.log("Step 1 is Shown - Single click to hide");
+}
+//step 2
+function hide2() {
+    document.getElementById("step2_div").style.display = "none";
+    console.log("Step 2 is Hidden - Double click to show");
+}
+
+function show2() {
+    document.getElementById("step2_div").style.display = "inline-block";
+    console.log("Step 2 is Shown - Single click to hide");
+}
+//step 3
+function hide3() {
+    document.getElementById("step3_div").style.display = "none";
+    console.log("Step 3 is Hidden - Double click to show");
+}
+
+function show3() {
+    document.getElementById("step3_div").style.display = "inline-block";
+    console.log("Step 3 is Shown - Single click to hide");
+}
+
+//step 4
+function hide4() {
+    document.getElementById("step4_div").style.display = "none";
+    console.log("Step 4 is Hidden - Double click to show");
+}
+
+function show4() {
+    document.getElementById("step4_div").style.display = "inline-block";
+    console.log("Step 4 is Shown - Single click to hide");
+}
+
+//step 5
+function hide5() {
+    document.getElementById("step5_div").style.display = "none";
+    console.log("Step 5 is Hidden - Double click to show");
+}
+
+function show5() {
+    document.getElementById("step5_div").style.display = "inline-block";
+    console.log("Step 5 is Shown - Single click to hide");
+}
+
+//step 6
+function hide6() {
+    document.getElementById("step6_div").style.display = "none";
+    console.log("Step 6 is Hidden - Double click to show");
+}
+
+function show6() {
+    document.getElementById("step6_div").style.display = "inline-block";
+    console.log("Step 6 is Shown - Single click to hide");
+}
